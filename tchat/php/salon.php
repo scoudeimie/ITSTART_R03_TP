fbda0587536217b4bec11297cc2e8f6b56848338
scoudeimie <serge.coude@imie.fr>
2015-01-26 11:50:38 +0100

    Remaniement des fichiers du salon pour adapter le code de l'inscription vers la création de salon

--- tchat/php/salon.php
+++ tchat/php/salon.php
@@ -6,86 +6,33 @@
 	define('SALON_NOM_EXIST', 2);
 	
 	/**
-	 * Vérifie si le pseudo est correct ou non
+	 * Inscrit dans la base le nouveau salon
 	 *
-	 * Le pseudo doit contenir que A-Za-z0-9
-	 *
-	 * @param $pseudo Pseudo à vérifier
-	 * @return Vrai ou faux selon le pseudo
+	 * @param $salonNom Nom du salon à créer
+	 * @param $salonDate Date et heure d'ouveture du salon
+	 * @param $salonDuree Durée d'ouverture en minute 
+	 * @param $id_utilisateur Id de l'utilisateur créant le salon
+	 * @return Code de retour (bien passé, nom du salon existant)
 	 */
-	function checkPseudo($pseudo) {
-	
-		// Utilisation des expressions régulières
-		// return preg_match('/^[A-Za-z0-9]{4,50}$/', $pseudo);
-	
-		// Je mets chaque caractère dans un tableau
-		$tab = str_split($pseudo);
-		// Je parcours le tableau caractère par caractère
-		for($i = 0; $i < count($tab); $i++) {
-			// Je mets le caractère d'indice $i dans une variable $c
-			$c = $tab[$i];
-			// Si le caractère est compris entre
-			// 'A' et 'Z' ou 'a' et 'z' ou '0' et '9'
-			if (($c >= 'A' && $c <= 'z') ||
-			    ($c >= '0' && $c <= '9'))
-				// On continue la vérification...
-				continue;
-			else // Le caractère n'est pas conforme, on renvoie faux
-				return false;
-		}
-		// l'ensemble des caractères ont été vérifiés, on renvoie donc vrai
-		return true;
-	} // Fin de la fonction checkPseudo
-	
-	/**
-	 * Vérifie si les mots de passe concordent
-	 *
-	 * Les mots de passe doivent être identiques et non vides
-	 * 
-	 * @param $mdp1 Mot de passe original
-	 * @param $mdp2 Mot de passe de confirmation
-	 * @return Vrai si les mots de passe sont identiques et non vide, faux sinon
-	 */
-	function checkMdp($mdp1, $mdp2) {
-		// les deux mots de passe doivent être identiques et non vide
-		return ($mdp1 == $mdp2 && $mdp1 != ""); 
-	} // Fin de la fonction checkMdp
-	
-	/**
-	 * Vérifie si le courriel est bien formé ou non
-	 *
-	 * @param $courriel Courriel à tester
-	 * @return vrai ou faux suivant le courriel passé
-	 */
-	function checkCourriel($courriel) {
-		return filter_var($courriel, FILTER_VALIDATE_EMAIL);
-	}
-	
-	/**
-	 * Inscrit dans la base le nouvel utilisateur
-	 *
-	 * @param $pseudo Pseudo à insérer
-	 * @param $mdp Mot de passe
-	 * @param $email Courriel de l'utilisateur
-	 * @param $profil Id du profil demandé
-	 * @return Code de retour (bien passé, pseudo existant, courriel existant)
-	 */
-	function inscription($pseudo, $mdp, $email, $profil) {
+	function creerSalon($salonNom, 
+						$salonDate, 
+						$salonDuree, 
+						$id_utilisateur = 1) {
 		// Chargement de la configuration
 		$dbConf = chargeConfiguration();
 		// Connexion à la base de données
 		$pdo = cnxBDD($dbConf);
 		// Exécution de la requête
 		// Je définie le "modèle" de ma requête
-		$req = "INSERT INTO Utilisateur (pseudo, password, email, id_profil) " .
-			   "VALUES (:pseudo, :password, :email, :profil);";
+		$req = "INSERT INTO salon (nom, ouverture, duree, id_utilisateur) " .
+			   "VALUES (:nom, :ouverture, :duree, :id_utilisateur);";
 		// Je prépare ma requête et j'obtient un objet la représentant
 		$pdoStmt = $pdo->prepare($req);
 		// J'associe à ma requête le contenu des variables
-		$pdoStmt->bindParam(':pseudo', $pseudo);
-		$pdoStmt->bindParam(':password', $mdp);
-		$pdoStmt->bindParam(':email', $email);
-		$pdoStmt->bindParam(':profil', $profil);
+		$pdoStmt->bindParam(':nom', $salonNom);
+		$pdoStmt->bindParam(':ouverture', $salonDate);
+		$pdoStmt->bindParam(':duree', $salonDuree);
+		$pdoStmt->bindParam(':id_utilisateur', $id_utilisateur);
 		// J'exécute ma requête
 		try {
 			$pdoStmt->execute();
@@ -94,14 +41,7 @@
 			$codeErr = $e->getCode();
 			switch($codeErr) {
 				case 23000: // C'est une valeur déjà présente dans la table
-					// "pseudo" n'est pas présent dans le message
-					if (strpos($e->getMessage(), "pseudo") === false ) {
-						// C'est donc le courriel qui est déjà présent
-						return INSCR_EMAIL_EXIST;
-					} else {
-						// C'est bien le pseudo qui existe déjà
-						return INSCR_PSEUDO_EXIST;
-					}
+					return SALON_NOM_EXIST;
 				default:	
 					// juste pour d'éventuelles gestions de nouvelles erreurs
 					die($e->getCode() . " / " . $e->getMessage());
@@ -109,8 +49,8 @@
 		}
 		$pdoStmt = NULL; // On "désalloue" l'objet représentant la requête
 		$pdo = NULL; // On "désalloue" l'objet de la connexion -> fin de la cnx
-		return INSCR_OK; // Tout s'est bien passé, on renvoie "OK"
-	} // Fin de la fonction inscription
+		return SALON_CREATION_OK; // Tout s'est bien passé, on renvoie "OK"
+	} // Fin de la fonction creerSalon
 	
 	/**
 	 * Renvoyer la liste des durées d'ouverture <option>
@@ -149,39 +89,21 @@
 	}
 	
 	// Si il y a eu soumission de formulaire
-	if (isset($_POST["pseudo"])) {
-		// Alors on procède à l'inscription
-		if (checkPseudo($_POST["pseudo"])) {
-			// on continue à checker
-			if (checkMdp($_POST["mdp"], $_POST["mdp2"])) {
-				if (checkCourriel($_POST["email"])) {
-					$res = inscription($_POST["pseudo"], 
-									md5($_POST["mdp"]),
-									$_POST["email"],
-									$_POST["profil"]);
-					switch($res) {
-						case INSCR_OK:
-							afficheResultat("Vous avez &eacute;t&eacute; bien inscrit !",
-											true);
-						case INSCR_PSEUDO_EXIST:
-							afficheResultat("Votre pseudo est d&eacute;j&agrave; pr&eacute;sent...",
-											false);
-						case INSCR_EMAIL_EXIST:
-							afficheResultat("Votre courriel est d&eacute;j&agrave; utilis&eacute;...",
-											false);
-					}		
-				} else {
-					afficheResultat("le courriel est mal form&eacute;...",
-									false);
-				}
-			} else {
-				afficheResultat("les mots de passe ne correspondent pas...",
+	if (isset($_POST["salonNom"])) {
+		// Alors on procède à la création du salon
+		$res = creerSalon($_POST["salonNom"], 
+						  $_POST["salonDate"]),
+						  $_POST["salonDuree"]);
+						  /* TODO 
+							Ajouter l'id de l'utilisateur
+						  */	
+		switch($res) {
+			case SALON_CREATION_OK:
+				afficheResultat("Le salon a bien &eacute;t&eacute; cr&eacute;&eacute;",
+								true);
+			case SALON_NOM_EXIST:
+				afficheResultat("Un salon de m&ecirc;me nom est d&eacute;j&agrave; pr&eacute;sent...",
 								false);
-			}	
-		} else {
-			afficheResultat("le pseudo n'est pas correct...",
-							false);
-		}
 	} else {
 		// Si pas de soumission de formulaire, on affiche le formulaire
 		$listeDurees = creerListeDureesOuveture();
